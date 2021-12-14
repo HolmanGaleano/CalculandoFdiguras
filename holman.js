c18db572bed4ca97c84dda42da6b0e106547c8aa
HolmanGaleano <mrholmanc@gmail.com>
2021-12-13 22:55:47 -0500

    cuadrado

--- holman.js
+++ holman.js
@@ -1,1 +1,8 @@
-console.log("Hola, mundo");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + " cm2");

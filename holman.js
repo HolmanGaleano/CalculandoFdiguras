8d1e11b5d779f4e19e1b8aa0cc97e06bc20d8f27
HolmanGaleano <mrholmanc@gmail.com>
2021-12-14 21:47:04 -0500

    Cambios 14 de diciembre

--- holman.js
+++ holman.js
@@ -1,3 +1,5 @@
+console.group("Cuadrados");
+
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
 
@@ -6,3 +8,43 @@ console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El área del cuadrado es: " + areaCuadrado + " cm2");
+
+console.groupEnd();
+
+//Triangulo
+
+console.group("Triángulos");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cm, " + ladoTriangulo2 + " cm, " + baseTriangulo + " cm");
+
+const alturaTriangulo = 5.5;
+
+console.log("La altura del triángulo mide: " + alturaTriangulo + " cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
+
+const areaTringulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triángulo es: " + areaTringulo + " cm2")
+
+
+console.groupEnd();
+
+
+console.log("Círculos");
+
+//Radio
+
+//Diámetro
+
+//PI
+
+//Circunferencia
+
+//Área
+
+console.groupEnd();

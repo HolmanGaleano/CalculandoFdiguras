e9369a455629acf55b6f9bb4fe2c4823dd11b0b1
HolmanGaleano <mrholmanc@gmail.com>
2021-12-15 22:42:21 -0500

    cambios 15 diciembre

--- holman.js
+++ holman.js
@@ -1,35 +1,43 @@
 console.group("Cuadrados");
 
-const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+// const ladoCuadrado = 5;
+// console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+perimetroCuadrado();
+// console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El área del cuadrado es: " + areaCuadrado + " cm2");
+function areaCuadrado(lado) {
+    return lado * lado;
+} 
+// console.log("El área del cuadrado es: " + areaCuadrado + " cm2");
 
 console.groupEnd();
 
 //Triangulo
-
 console.group("Triángulos");
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
+// const ladoTriangulo1 = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
 
-console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cm, " + ladoTriangulo2 + " cm, " + baseTriangulo + " cm");
+// console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " cm, " + ladoTriangulo2 + " cm, " + baseTriangulo + " cm");
 
-const alturaTriangulo = 5.5;
+// const alturaTriangulo = 5.5;
 
-console.log("La altura del triángulo mide: " + alturaTriangulo + " cm");
+// console.log("La altura del triángulo mide: " + alturaTriangulo + " cm");
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+perimetroTriangulo();
+// console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log("El perímetro del triángulo es: " + perimetroTriangulo + " cm");
-
-const areaTringulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log("El área del triángulo es: " + areaTringulo + " cm2")
+function areaTringulo(base, altura) {
+    return (base * altura) / 2;
+}
+// console.log("El área del triángulo es: " + areaTringulo + " cm2")
 
 
 console.groupEnd();
